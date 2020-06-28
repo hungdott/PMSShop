cdb586090536cd3935d98049f591a58392f8849f
hungdo <Admin@DESKTOP-UMQ13TK>
2020-06-28 15:42:48 +0700

    Add project files.

---
+++ PMSShop.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PMSShop.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
