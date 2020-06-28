2950eec303af8c69292cae7143356150cc8bc34c
hungdo <Admin@DESKTOP-UMQ13TK>
2020-06-28 18:23:18 +0700

    design database

---
+++ PMSShop.Data/EF/PMSShopDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PMSShop.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PMSShop.Data.EF
+{
+    class PMSShopDbContext : DbContext
+    {
+        public PMSShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
