c03b7db4ed9499ab802333e936765f518208da95
hungdo <Admin@DESKTOP-UMQ13TK>
2020-07-06 18:55:56 +0700

    add jwt token to swagger

--- PMSShop.Application/Catalog/Products/PublicProductService.cs
+++ PMSShop.Application/Catalog/Products/PublicProductService.cs
@@ -15,6 +15,7 @@ namespace PMSShop.Application.Catalog.Products
     public class PublicProductService : IPublicProductService
     {
         private readonly PMSShopDbContext _context;
+
         public PublicProductService(PMSShopDbContext context)
         {
             _context = context;
@@ -23,46 +24,50 @@ namespace PMSShop.Application.Catalog.Products
         public async Task<List<ProductViewModel>> GetAll(string languageId)
         {
             var query = from p in _context.Products
-                         join pt in _context.ProductTranslations on p.Id equals pt.ProductId
-                         join pic in _context.ProductInCategories on p.Id equals pic.ProductId
-                         join ct in _context.Categories on pic.CategoryId equals ct.Id
-                         where pt.LanguageId.Equals(languageId)
-                         //where pt.Name.Contains(request.Keyword)
-                         select new { p, pt, pic };
-                         
-           
+                        join pt in _context.ProductTranslations on p.Id equals pt.ProductId
+                        join pic in _context.ProductInCategories on p.Id equals pic.ProductId
+                        join ct in _context.Categories on pic.CategoryId equals ct.Id
+                        where pt.LanguageId.Equals(languageId)
+                        //where pt.Name.Contains(request.Keyword)
+                        select new { p, pt, pic };
+
             var data = await query.Select(x => new ProductViewModel()
-                            {
-                                Id = x.p.Id,
-                                Name = x.pt.Name,
-                                Description = x.pt.Description,
-                                DateCreated = x.p.DateCreated,
-                                Details = x.pt.Details,
-                                LanguageId = x.pt.LanguageId,
-                                OriginalPrice = x.p.OriginalPrice,
-                                Price = x.p.Price,
-                                SeoAlias = x.pt.SeoAlias,
-                                SeoDescription = x.pt.SeoDescription,
-                                SeoTitle = x.pt.SeoTitle,
-                                Stock = x.p.Stock,
-                                ViewCount = x.p.ViewCount
-                            }).ToListAsync();
+            {
+                Id = x.p.Id,
+                Name = x.pt.Name,
+                Description = x.pt.Description,
+                DateCreated = x.p.DateCreated,
+                Details = x.pt.Details,
+                LanguageId = x.pt.LanguageId,
+                OriginalPrice = x.p.OriginalPrice,
+                Price = x.p.Price,
+                SeoAlias = x.pt.SeoAlias,
+                SeoDescription = x.pt.SeoDescription,
+                SeoTitle = x.pt.SeoTitle,
+                Stock = x.p.Stock,
+                ViewCount = x.p.ViewCount
+            }).ToListAsync();
             return data;
         }
 
         public async Task<PagedResult<ProductViewModel>> GetAllByCategoryID(string languageId, GetPublicProductPagingRequest request)
         {
-            var query = (from p in _context.Products
-                         join pt in _context.ProductTranslations on p.Id equals pt.ProductId
-                         join pic in _context.ProductInCategories on p.Id equals pic.ProductId
-                         join ct in _context.Categories on pic.CategoryId equals ct.Id
-                         where pt.LanguageId.Equals(languageId)
-                         //where pt.Name.Contains(request.Keyword)
-                         select new { p, pt, pic })
-                         .WhereIf(request.CategoryId.HasValue
-                                  && request.CategoryId.Value > 0,
-                                  x => request.CategoryId == x.pic.CategoryId)
-                         .AsQueryable();
+            var query = from p in _context.Products
+                        join pt in _context.ProductTranslations on p.Id equals pt.ProductId
+                        join pic in _context.ProductInCategories on p.Id equals pic.ProductId
+                        join ct in _context.Categories on pic.CategoryId equals ct.Id
+                        where pt.LanguageId.Equals(languageId)
+                        //where pt.Name.Contains(request.Keyword)
+                        select new { p, pt, pic };
+            if (request.CategoryId.HasValue && request.CategoryId.Value > 0)
+            {
+                query = query.Where(x => x.pic.CategoryId == request.CategoryId);
+            }
+            //.WhereIf(request.CategoryId.HasValue
+            //         && request.CategoryId.Value > 0,
+            //         x => request.CategoryId == x.pic.CategoryId)
+            //.AsQueryable();
+
             int totalRow = await query.CountAsync();
             var data = query.Skip((request.PageIndex - 1) * request.PageSize)
                             .Take(request.PageSize)
@@ -89,7 +94,5 @@ namespace PMSShop.Application.Catalog.Products
             };
             return pageResult;
         }
-
-       
     }
 }
--- PMSShop.BackendApi/Controllers/ProductsController.cs
+++ PMSShop.BackendApi/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PMSShop.Application.Catalog.Products;
@@ -12,6 +13,7 @@ namespace PMSShop.BackendApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ProductsController : ControllerBase
     {
         private readonly IPublicProductService _publicProductService;
